af8ff1ea4e4a47f0da7d8135f4b94ef27efb4f62
dloyolar <dieegolr@gmail.com>
2021-12-18 16:38:39 -0300

    feat: add LoginScreen with navigate

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
 export const LoginScreen = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/marvel', {
+      replace: true,
+    });
+  };
+
   return (
-    <div>
-      <h1>LoginScreen</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <p className="text-muted">
+        (Don't worry, this is a fake login, you dont need any user or
+        credentials 👌)
+      </p>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 };
