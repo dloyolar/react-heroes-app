49bf5c8978962b684f8ed5e75992cf34307a6c41
dloyolar <dieegolr@gmail.com>
2021-12-19 20:33:00 -0300

    feat: add new HeroCard component

---
+++ src/components/hero/HeroCard.js
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+
+import './HeroCard.css';
+
+export const HeroCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
+  const imagePath = `/assets/${id}.jpg`;
+
+  return (
+    <Link to={`/hero/${id}`} className="my-card">
+      <img src={imagePath} className="img img-responsive" alt={superhero} />
+      <div className="profile-name">{superhero}</div>
+      <div className="profile-position">{alter_ego}</div>
+      <div className="profile-overview">
+        <div className="profile-overview">
+          <div className="row">
+            <div className="col-ms-4">
+              <h3>{publisher}</h3>
+              <p className="fw-bold">
+                First appaerance: <br />
+                {first_appearance}
+              </p>
+              {alter_ego !== characters && <p>{characters}</p>}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
