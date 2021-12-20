a21f979e151395613b35cd60259a18a104212878
dloyolar <dieegolr@gmail.com>
2021-12-19 21:40:34 -0300

    feat: get params of url and use it with getHeroById function

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,18 @@
+import { useParams, Navigate } from 'react-router-dom';
+import { getHeroById } from '../../helpers/getHeroById';
+
 export const HeroScreen = () => {
+  const { heroId } = useParams();
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <h1>Hero Screen</h1>
+      <p>{hero.superhero}</p>
     </div>
   );
 };
