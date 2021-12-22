d6b7f022bfc3712c9e5936e686ebe2f5396d62a9
dloyolar <dieegolr@gmail.com>
2021-12-22 20:56:43 -0300

    refactor: use queryString to search heros and get params of url

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,15 +1,28 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+
+import { getHeroesByName } from '../../helpers/getHeroesByName';
 import { useForm } from '../../hooks/useForm';
+import { HeroCard } from '../hero/HeroCard';
 
 export const SearchScreen = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse(location.search);
+
   const [formValues, handleInputChange] = useForm({
-    searchText: '',
+    searchText: q,
   });
 
   const { searchText } = formValues;
 
+  const heroesFiltered = getHeroesByName(q);
+
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log(searchText);
+
+    navigate(`?q=${searchText}`);
   };
 
   return (
@@ -40,6 +53,14 @@ export const SearchScreen = () => {
               Search
             </button>
           </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          {heroesFiltered.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
