996f3b0c0593b9ca9cc7b7d8fd824042ef4ff47f
dloyolar <dieegolr@gmail.com>
2021-12-26 18:13:06 -0300

    refactor: add context to login and logout

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,18 @@
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../auth/authContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = () => {
   const navigate = useNavigate();
+  const { dispatch } = useContext(AuthContext);
 
   const handleLogin = () => {
+    const action = {
+      type: types.login,
+      payload: { name: 'Diego' },
+    };
+    dispatch(action);
     navigate('/marvel', {
       replace: true,
     });
