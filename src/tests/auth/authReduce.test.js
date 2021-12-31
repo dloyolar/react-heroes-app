5f45aa5503174caf426a1fcb3d38587f5c340353
dloyolar <dieegolr@gmail.com>
2021-12-30 22:59:19 -0300

    test: add authReducer test

---
+++ src/tests/auth/authReduce.test.js
@@ -0,0 +1,37 @@
+import { type } from '@testing-library/user-event/dist/type';
+import { authReducer } from '../../auth/authReducer';
+import { types } from '../../types/types';
+
+describe('Test on authReducer', () => {
+  test('should be return state by default', () => {
+    const state = authReducer({ logged: false }, {});
+    expect(state).toEqual({ logged: false });
+  });
+
+  test('should be auth and set name of user', () => {
+    const action = {
+      type: types.login,
+      payload: {
+        name: 'Diego',
+      },
+    };
+
+    const state = authReducer({ logged: false }, action);
+    expect(state).toEqual({
+      logged: true,
+      name: 'Diego',
+    });
+  });
+
+  test('should be delete the name and logged = false', () => {
+    const action = {
+      type: types.logout,
+    };
+
+    const state = authReducer({ logged: true, name: 'Diego' }, action);
+
+    expect(state).toEqual({
+      logged: false,
+    });
+  });
+});
