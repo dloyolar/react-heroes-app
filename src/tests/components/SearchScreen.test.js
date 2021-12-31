07a0c45cc2d221b7511002bc8a2f394e123681f5
dloyolar <dieegolr@gmail.com>
2021-12-31 11:08:59 -0300

    test: improve test on SearchScreen

--- src/tests/components/SearchScreen.test.js
+++ src/tests/components/SearchScreen.test.js
@@ -12,4 +12,27 @@ describe('Test on <SearchScreen />', () => {
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('.alert-info').text().trim()).toBe('Search a hero');
   });
+
+  test('should be display batman and input value with queryString', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman']}>
+        <SearchScreen />
+      </MemoryRouter>
+    );
+    expect(wrapper.find('input').prop('value')).toBe('batman');
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should be display an error if hero doesnt exists', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman123']}>
+        <SearchScreen />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.find('input').prop('value')).toBe('batman123');
+    expect(wrapper.find('.alert-danger').text().trim()).toBe(
+      'No results for: batman123'
+    );
+  });
 });
